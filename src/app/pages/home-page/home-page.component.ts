832f3d4966bee3a7c438fc0c08158ea393b4c4b1
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-07-04 19:59:41 +0530

    feat: initial commit

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
