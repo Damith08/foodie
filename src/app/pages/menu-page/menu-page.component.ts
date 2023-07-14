5bde0d4e4ecd5020b35ccd090f6e009e72f70575
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-07-14 20:34:45 +0530

    feat: footer padding changes
    changes in footer paddings

--- src/app/pages/menu-page/menu-page.component.ts
+++ src/app/pages/menu-page/menu-page.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-page',
   templateUrl: './menu-page.component.html',
-  styleUrls: ['./menu-page.component.scss']
+  styleUrls: ['./menu-page.component.scss'],
 })
-export class MenuPageComponent {
-
-}
+export class MenuPageComponent {}
