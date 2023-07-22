7cab6d7c2f3ce8cdab20dbcce5e1c869b715b65d
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-07-22 15:47:50 +0530

    feat: nav bar
    nav bar appearance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class AppComponent {
   title = 'deliveroo-clone';
   showFiller = false;
+  sidenav: any;
 
   openSideBar(sidebarOpen: boolean) {}
 }
