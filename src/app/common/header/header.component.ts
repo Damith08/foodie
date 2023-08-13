f4f1e095a998f8c90e0411c4aa68e5157e513d82
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-08-13 11:50:19 +0530

    feat: active router links
    active router link

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private router: Router) {}
+export class HeaderComponent implements OnInit {
+  isLoginPage: boolean = false;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
   @Output() sidenav: EventEmitter<any> = new EventEmitter();
   showFiller = false;
+
+  ngOnInit(): void {
+    // Check if the current route is the login page
+    this.route.url.subscribe((url) => {
+      this.isLoginPage = url.join('/') === 'login';
+    });
+  }
 
   toggle() {
     this.sidenav.emit();
