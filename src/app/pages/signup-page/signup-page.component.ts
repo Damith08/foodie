6520a2535f7e36eab790a3911be690453b4d07cd
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-11-09 21:23:12 +0530

    feat: add signup page

--- src/app/pages/signup-page/signup-page.component.ts
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
@@ -8,9 +9,14 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupPageComponent {
   public user = { email: '' };
-  email: any;
+  model: any = {};
+  firstName = new FormControl('', [Validators.required]);
+  lastName = new FormControl('', [Validators.required]);
+  username = new FormControl('', [Validators.required]);
+  address = new FormControl('', [Validators.required]);
+  contactNumber = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
 
-  onSubmit(form: NgForm) {
-    console.log('form');
-  }
+  onSubmit(form: NgForm) {}
 }
